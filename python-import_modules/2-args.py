b0c41cf72d5a6c46035fc8cfb9c06ae205c7b8f8
Meashyshy <camillefvrl@gmail.com>
2023-06-02 12:37:29 -0700

    added

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    sentence = "arguments."
+    if (len(args) > 1):
+        sentence = "arguments:"
+    elif (len(args) == 1):
+        sentence = "argument:"
+
+    print("{:} {:}".format(len(args), sentence))
+    for arg in range(len(args)):
+        print("{:}: {:}".format(arg + 1, args[arg]))
+
