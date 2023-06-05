a9f2106fbb14b4bf2966e564364c833f8fc7101a
Meashyshy <camillefvrl@gmail.com>
2023-06-05 06:21:53 -0700

    T1 - Completed

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx is None:
-        return None
-    if idx > len(my_list):
+    if idx is None or idx > len(my_list):
         return None
     return my_list[idx]
