4a76e0645f7c8029e9fdbeef34c4316006277fe2
Meashyshy <camillefvrl@gmail.com>
2023-06-05 05:13:40 -0700

    T0 - Prints all integers of a list (Completed)

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if my_list is None:
-        my_list = [1, 2, 3, 4, 5]
-
     for num in my_list:
-        print("{}".format(num))
+        print("{}".format(int(num)))
