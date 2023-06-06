87144c517d48136520a3abc8c4e96fdce029108d
Meashyshy <camillefvrl@gmail.com>
2023-06-06 13:28:42 -0700

    T4 - Use of copy() to replace without modifying the original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
         return new_list
-    for i in range(len(my_list)):
+    for i in range(len(new_list)):
         if i == idx:
-            my_list[i] = element
+            new_list[i] = element
             return new_list
