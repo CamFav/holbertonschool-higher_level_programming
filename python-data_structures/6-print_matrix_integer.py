4f047e5f8a472d2de8140f0e085a858121d2cd4b
Meashyshy <camillefvrl@gmail.com>
2023-06-07 05:43:53 -0700

    T6 - Prints a matrix of integers (Use of .join and map)

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return
-
     for i in matrix:
-        print(' '.join('{0}'.format(element) for element in i))
+        print(' '.join('{:d}'.format(element) for element in i))
