8e267e5b13e379accf8b09999223cbcaebd17f06
Meashyshy <camillefvrl@gmail.com>
2023-06-12 08:36:05 -0700

    T10 - Bigggest integer value / Use of max()

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) == 0:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-
     max_value = max(a_dictionary)
     return max_value
