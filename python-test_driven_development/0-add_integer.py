c9bb02175282ade924a11c548395d8c4b97659bb
Meashyshy <camillefvrl@gmail.com>
2023-06-15 01:44:19 -0700

    T0 - Integers addition with docttest

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,11 +4,13 @@
 
 def add_integer(a, b=98):
     """Add two integers."""
-    try:
-        result = a + b
-        return int(result)
-    except:
-        if type(a) not in [int, float]:
-            raise TypeError("a must be an integer")
-        else:
-            raise TypeError("b must be an integer")
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer or float")
+
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer or float")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
