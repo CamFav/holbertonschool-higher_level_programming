a969069ee069b21ed39deaf0af0cbf1702e893c1
Meashyshy <camillefvrl@gmail.com>
2023-06-19 07:27:42 -0700

    T3 - Check if is an instance of or not

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+    """
+    return isinstance(obj, a_class)
