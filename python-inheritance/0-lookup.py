85b69e2548f23a2111a92a8562d9597789cb5b75
Meashyshy <camillefvrl@gmail.com>
2023-06-19 06:54:04 -0700

    Returns the list of available attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""lookup
+"""
+
+
 def lookup(obj):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return dir(obj)
