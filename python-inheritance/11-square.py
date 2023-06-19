15796daed57cd964fb2f88e7d5e2a4006f7aa095
Meashyshy <camillefvrl@gmail.com>
2023-06-19 09:14:48 -0700

    T11 - Implemented str

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """_summary_
+
+    Args:
+        Rectangle (_type_): _description_
+    """
+    def __init__(self, size):
+        self.__size = super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {0}/{0}".format(self.__size)
