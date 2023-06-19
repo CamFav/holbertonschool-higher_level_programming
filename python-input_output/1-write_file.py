7eba1ec98749b17a0cc7dd3f736f8c485dcee46d
Meashyshy <camillefvrl@gmail.com>
2023-06-19 10:32:54 -0700

    T1 - Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,6 +9,6 @@ def write_file(filename="", text=""):
         filename (str, optional): _description_. Defaults to "".
         text (str, optional): _description_. Defaults to "".
     """
-    with open(filename, encoding="utf-8") as f:
-        readfile = f.write(text)
-        return len(readfile)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
