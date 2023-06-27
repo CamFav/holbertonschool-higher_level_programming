69ed107b7d5b5e0236ccbeee09d26c022be3d916
Meashyshy <camillefvrl@gmail.com>
2023-06-27 04:23:59 -0700

    T10 - Student to JSN with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """_summary_
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        if attrs is None:
+            return self.__dict__
+
+        student_dict = {}
+        for attr, value in self.__dict__.items():
+            if attr in attrs:
+                student_dict[attr] = value
+
+        return student_dict
