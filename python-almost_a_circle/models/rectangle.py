08ef449ec6537f89a92a8d7f28475099e6991f80
Meashyshy <camillefvrl@gmail.com>
2023-06-29 03:51:22 -0700

    Corrected instance attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,8 +19,8 @@ class Rectangle(Base):
             y (int, optional): _description_. Defaults to 0.
             id (_type_, optional): _description_. Defaults to None.
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
