bb99d3906e95d0f9e1a4c6a1254fb82ba0eb604b
Meashyshy <camillefvrl@gmail.com>
2023-07-03 04:10:32 -0700

    Update with args and kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,15 +85,53 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
 {self.width}/{self.height}"
 
-    def update(self, *args):
-        """Assign args to attributes in specified order"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        """Update the Rectangle.
+        Args:
+            *args (ints): New keyibute values.
+                - 1st argument represents id keyibute
+                - 2nd argument represents width keyibute
+                - 3rd argument represent height keyibute
+                - 4th argument represents x keyibute
+                - 5th argument represents y keyibute
+            **kwargs (dict): New key/value pairs of keyibutes.
+        """
+        if args and len(args) != 0:
+            for index, arg in enumerate(args):
+                if index == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+
+                elif index == 1:
+                    self.width = arg
+
+                elif index == 2:
+                    self.height = arg
+
+                elif index == 3:
+                    self.x = arg
+
+                elif index == 4:
+                    self.y = arg
+
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = value
+
+                elif key == "width":
+                    self.width = value
+
+                elif key == "height":
+                    self.height = value
+
+                elif key == "x":
+                    self.x = value
+
+                elif key == "y":
+                    self.y = value
