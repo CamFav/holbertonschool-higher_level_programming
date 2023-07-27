1690904e27a970694528c786737ccb4339198bab
Meashyshy <camillefvrl@gmail.com>
2023-07-27 06:39:46 -0700

    added apostrophes

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- lists the number of records with the same score
-SELECT score, count(*) as number FROM second_table
-ORDER BY score DESC;
+SELECT score, count(*) as 'number' FROM second_table
+ORDER BY number DESC;
