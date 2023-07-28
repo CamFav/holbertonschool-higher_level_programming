6d970aef7a8099597ffd424389a0418847550f0a
Meashyshy <camillefvrl@gmail.com>
2023-07-28 03:38:36 -0700

    fix wrong table name

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Creates a table with a id that must be unique.
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 PRIMARY KEY,
     name varchar(256)
 );
