ad75e8e28d7c0f0a9922a2f485b3e46ed0483191
Meashyshy <camillefvrl@gmail.com>
2023-07-28 03:28:47 -0700

    read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- Creates a database and a user with specific access
-CREATE TABLE IF NOT EXISTS force_name (
-    id int,
-    name varchar(256) NOT NULL
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
