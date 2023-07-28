5937bd606094121b403c90ca6e9579c5439aa210
Meashyshy <camillefvrl@gmail.com>
2023-07-28 03:44:20 -0700

    added use statement

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,8 @@
 -- Script that creates a database and a table with specific attributes.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name varchar(256) NOT NULL
