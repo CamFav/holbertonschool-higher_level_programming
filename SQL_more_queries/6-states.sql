2cd9371c9ad4e23edb648fad7495042fc8bb22b7
Meashyshy <camillefvrl@gmail.com>
2023-07-28 03:42:26 -0700

    creates a database and a table with specific attributes

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Script that creates a database and a table with specific attributes.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(256) NOT NULL
+);
