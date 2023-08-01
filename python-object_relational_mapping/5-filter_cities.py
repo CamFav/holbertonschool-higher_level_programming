7e9a5bc948fff1556646be058e468962c6216602
Meashyshy <camillefvrl@gmail.com>
2023-08-01 08:31:27 -0700

    cleared

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,15 +9,14 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3])
-    state_name = sys.argv[4]
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities INNER JOIN states \
-                ON states.id = cities.state_id WHERE states.name = '{}'"
-            .format(state_name)
-            )
+    cur.execute("SELECT cities.name FROM cities LEFT JOIN states \
+                ON states.id=cities.state_id WHERE states.name=%s",
+                (sys.argv[4],))
     rows = cur.fetchall()
 
     cities_str = ", ".join(row[0] for row in rows)
     print(cities_str)
+
     cur.close()
     db.close()
