652c54e39982dc0c1a8fb63bc2da40e4e93609cf
Meashyshy <camillefvrl@gmail.com>
2023-08-01 04:09:09 -0700

    added \ to separate 2 actions

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,11 +15,10 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
-
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
